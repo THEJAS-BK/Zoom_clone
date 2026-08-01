5d838082a0cdd51a020699b4e39ff6cf659f9e31
THEJAS-BK <thejasbk998@gmail.com>
2026-08-01 11:25:19 -0300

    testing

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-export const socket: Socket = io(import.meta.env.SOCKET_URL , {
+export const socket: Socket = io(import.meta.env.VITE_SOCKET_URL , {
   autoConnect: false,
   auth:(cb) => {
     cb({ accesstoken: localStorage.getItem("accessToken") });
